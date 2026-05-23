c95fcb019ddcd4c78dd20d1972ee46890cf6d8d8
AK360-cloud <achuananthan335@gmail.com>
2026-05-24 00:30:45 +0530

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
